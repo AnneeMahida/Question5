b3cad6a5a0659ef2f3657e97b7b8cc10889a6c7d
Annee Mahida <Annee Mahida@LAPTOP-L5E45TAD>
2023-06-22 12:16:26 -0400

    Add project files.

---
+++ Question5/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace ProblemSet
+{
+    class Question5
+    {
+        static void Main(string[] args)
+        {
+            Double a, a1, a2, b, b1, b2;
+
+            Console.WriteLine("Enter the point, lower left and upper right corner of the rectangle: ");
+            a = Convert.ToDouble(Console.ReadLine());
+            b = Convert.ToDouble(Console.ReadLine());
+            a1 = Convert.ToDouble(Console.ReadLine());
+            b1 = Convert.ToDouble(Console.ReadLine());
+            a2 = Convert.ToDouble(Console.ReadLine());
+            b2 = Convert.ToDouble(Console.ReadLine());
+
+            bool decision = Inside(a, b, a1, b1, a2, b2);
+
+            Console.WriteLine(decision);
+            Console.ReadLine();
+        }
+
+        static bool Inside(Double x, Double y, Double x1, Double y1, Double x2, Double y2)
+        {
+            if (x > x1 && x < x2 && y > y1 && y < y2)
+                return true;
+            else
+                return false;
+        }
+
+    }
+}
